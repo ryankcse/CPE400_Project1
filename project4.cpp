5ef865d0c724ff6e7bdd37c84d40da48bc7c9fbe
Ryan Kim <ryan44544@gmail.com>
2018-12-01 16:43:22 -0800

    First Commit

---
+++ project4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+  int inf = INT_MAX;
+  int graph[7][7] = {0, 3, 5, 7, inf, inf, inf
+                     3, 0, inf, inf, 2, inf, inf,
+                     5, inf, 0, inf, 4, 6, inf,
+                     7, inf, inf, 0, inf, 8, inf,
+                     inf, 2, 4, inf, 0, 9, 1,
+                     inf, inf, inf, inf, 1, inf, 0
+                    };
+
+
+
+
+}
